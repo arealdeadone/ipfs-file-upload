35509c846ff3e2761bb9d53445df11efc441eef1
Arvind <ari0997@gmail.com>
2018-06-17 17:35:56 +0530

    My first dApp

---
+++ src/storehash.js
@@ -0,0 +1,36 @@
+import web3 from './web3';
+
+const address = '0x4a1e9ef0f0909dc2b2bd7c95906d1e9dec079210';
+
+const abi = [
+    {
+        "constant": false,
+        "inputs": [
+            {
+                "name": "x",
+                "type": "string"
+            }
+        ],
+        "name": "sendHash",
+        "outputs": [],
+        "payable": false,
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+        "constant": true,
+        "inputs": [],
+        "name": "getHash",
+        "outputs": [
+            {
+                "name": "x",
+                "type": "string"
+            }
+        ],
+        "payable": false,
+        "stateMutability": "view",
+        "type": "function"
+    }
+];
+
+export default new web3.eth.Contract(abi, address);
